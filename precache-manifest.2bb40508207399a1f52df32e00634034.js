9716eb97688d275d2477e7e3538a46f0e1b0d94b
esleptsova <Unik.SE>
2020-06-30 10:56:14 +0100

    Updates

---
+++ precache-manifest.2bb40508207399a1f52df32e00634034.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b2da42b80588bbaac5cea11c868791",
+    "url": "/sort-image-app/index.html"
+  },
+  {
+    "revision": "c6f29f3f2a9b5b1b9a65",
+    "url": "/sort-image-app/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "110734de4d225d8d3655",
+    "url": "/sort-image-app/static/css/main.fe7097d2.chunk.css"
+  },
+  {
+    "revision": "c6f29f3f2a9b5b1b9a65",
+    "url": "/sort-image-app/static/js/2.24072b61.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sort-image-app/static/js/2.24072b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110734de4d225d8d3655",
+    "url": "/sort-image-app/static/js/main.841a0d13.chunk.js"
+  },
+  {
+    "revision": "48cf648678a832a10312",
+    "url": "/sort-image-app/static/js/runtime-main.9d958a11.js"
+  },
+  {
+    "revision": "d2fdbd06e114e00eba65432e80dda674",
+    "url": "/sort-image-app/static/media/baloon.d2fdbd06.gif"
+  },
+  {
+    "revision": "9f3113c9d402ec331f1fb1470a117e14",
+    "url": "/sort-image-app/static/media/bear.9f3113c9.png"
+  },
+  {
+    "revision": "9a21c0936d45e0aef866219afc9b9abf",
+    "url": "/sort-image-app/static/media/cat.9a21c093.png"
+  },
+  {
+    "revision": "4fea58d86f91d0effad17d5999263b0b",
+    "url": "/sort-image-app/static/media/dolphin.4fea58d8.png"
+  },
+  {
+    "revision": "9d2e33c1f9aebfce56b8c9606a4c8364",
+    "url": "/sort-image-app/static/media/elefant.9d2e33c1.png"
+  },
+  {
+    "revision": "55b06dbfed17a2205887ec4ea7667fb7",
+    "url": "/sort-image-app/static/media/panda.55b06dbf.png"
+  },
+  {
+    "revision": "7e5e62be3ca3eade6bfb5b254d9fa499",
+    "url": "/sort-image-app/static/media/rule1.7e5e62be.png"
+  }
+]);
